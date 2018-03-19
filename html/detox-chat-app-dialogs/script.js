1cafba1efcbe90ce0febea74622171c67849d4ae
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2018-03-19 06:42:50 +0200

    Added top panel in dialogs window, friend's nickname is shown there for now

--- html/detox-chat-app-dialogs/script.js
+++ html/detox-chat-app-dialogs/script.js
@@ -13,27 +13,33 @@
         type: Boolean,
         value: false
       },
+      contact: Object,
       messages: Array
     },
     ready: function(){
       var this$ = this;
       Promise.all([require(['@detox/utils']), this._state_instance_ready]).then(function(arg$){
-        var detoxUtils, state;
+        var detoxUtils, are_arrays_equal, state;
         detoxUtils = arg$[0][0];
+        are_arrays_equal = detoxUtils.are_arrays_equal;
         state = this$._state_instance;
         state.on('ui_active_contact_changed', function(new_active_contact){
           this$.active_contact = true;
+          this$.contact = state.get_contact(new_active_contact);
           this$.messages = state.get_contact_messages(new_active_contact).slice();
           this$.notifyPath('messages');
+          this$.$['send-form'].querySelector('textarea').value = '';
         }).on('contact_messages_changed', function(contact_id){
           var active_contact;
           active_contact = state.get_ui_active_contact();
-          if (active_contact && detoxUtils.are_arrays_equal(contact_id, active_contact)) {
+          if (active_contact && are_arrays_equal(contact_id, active_contact)) {
             this$.messages = state.get_contact_messages(contact_id).slice();
             this$.notifyPath('messages');
           }
-        }).on('ui_active_contact_changed', function(contact_id){
-          this$.$['send-form'].querySelector('textarea').value = '';
+        }).on('contact_changed', function(new_contact){
+          if (are_arrays_equal(this$.contact.id, new_contact.id)) {
+            this$.contact = new_contact;
+          }
         });
       });
     },
--- html/detox-chat-app-dialogs/script.ls
+++ html/detox-chat-app-dialogs/script.ls
@@ -12,27 +12,33 @@ Polymer(
 		active_contact	:
 			type	: Boolean
 			value	: false
+		contact			: Object
 		messages		: Array
 	ready : !->
 		Promise.all([
 			require(['@detox/utils'])
 			@_state_instance_ready
 		]).then ([[detox-utils]]) !~>
-			state	= @_state_instance
+			are_arrays_equal	= detox-utils.are_arrays_equal
+
+			state				= @_state_instance
 			state
 				.on('ui_active_contact_changed', (new_active_contact) !~>
 					@active_contact	= true
+					@contact		= state.get_contact(new_active_contact)
 					@messages		= state.get_contact_messages(new_active_contact).slice() # TODO: slice is a hack until https://github.com/Polymer/polymer/issues/5151 is fixed
 					@notifyPath('messages')
+					@$['send-form']querySelector('textarea').value	= '' # TODO: Same on per-contact basis instead of erasing
 				)
 				.on('contact_messages_changed', (contact_id) !~>
 					active_contact	= state.get_ui_active_contact()
-					if active_contact && detox-utils.are_arrays_equal(contact_id, active_contact)
+					if active_contact && are_arrays_equal(contact_id, active_contact)
 						@messages	= state.get_contact_messages(contact_id).slice() # TODO: slice is a hack until https://github.com/Polymer/polymer/issues/5151 is fixed
 						@notifyPath('messages')
 				)
-				.on('ui_active_contact_changed', (contact_id) !~>
-					@$['send-form']querySelector('textarea').value	= ''
+				.on('contact_changed', (new_contact) !~>
+					if are_arrays_equal(@contact.id, new_contact.id)
+						@contact	= new_contact
 				)
 	_send : !->
 		state			= @_state_instance
