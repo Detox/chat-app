0ad803ae548bdd9fe4b3fc9abb111eba5863aab4
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2018-05-30 13:40:55 +0300

    Simplified gulpfile by removing variables and specifying paths explicitly. Fresh minified build.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@
  * @license 0BSD
  */
 (function(){
-  var cleanCss, crypto, del, exec, fs, gulp, gulpHtmlmin, gulpRename, gulpRequirejsOptimize, runSequence, uglifyEs, uglify, workboxBuild, minify_css, instance, minify_js, file_hash, AUDIO_REGEXP, FONTS_REGEXP, IMAGES_REGEXP, SCRIPTS_REGEXP, BLACKLISTED_FONTS, FA_FONT, BUNDLED_CSS, BUNDLED_HTML, BUNDLED_JS, BUNDLED_MANIFEST, BUNDLED_SW, DESTINATION, MINIFIED_CSS, MINIFIED_HTML, MINIFIED_JS, MINIFIED_SW, SOURCE_CSS, SOURCE_HTML, SOURCE_MANIFEST, requirejs_config;
+  var cleanCss, crypto, del, exec, fs, gulp, gulpHtmlmin, gulpRename, gulpRequirejsOptimize, runSequence, uglifyEs, uglify, workboxBuild, minify_css, instance, minify_js, file_hash, AUDIO_REGEXP, FONTS_REGEXP, IMAGES_REGEXP, SCRIPTS_REGEXP, BLACKLISTED_FONTS, requirejs_config;
   cleanCss = require('clean-css');
   crypto = require('crypto');
   del = require('del');
@@ -51,20 +51,6 @@
   IMAGES_REGEXP = /url\(\.\.\/img\/.+?\)/g;
   SCRIPTS_REGEXP = /<script>[^]+?<\/script>\n*/g;
   BLACKLISTED_FONTS = ['fa-regular-400.woff2', 'fa-brands-400.woff2'];
-  FA_FONT = 'fa-solid-900.woff2';
-  BUNDLED_CSS = 'style.css';
-  BUNDLED_HTML = 'index.html';
-  BUNDLED_JS = 'script.js';
-  BUNDLED_MANIFEST = 'manifest.json';
-  BUNDLED_SW = 'sw.js';
-  DESTINATION = 'dist';
-  MINIFIED_CSS = 'style.min.css';
-  MINIFIED_HTML = 'index.min.html';
-  MINIFIED_JS = 'script.min.js';
-  MINIFIED_SW = 'sw.min.js';
-  SOURCE_CSS = 'css/style.css';
-  SOURCE_HTML = 'html/index.html';
-  SOURCE_MANIFEST = 'manifest.json';
   requirejs_config = {
     'baseUrl': '.',
     'paths': {
@@ -116,10 +102,10 @@
     ]
   };
   gulp.task('bundle-clean', function(){
-    return del([DESTINATION + "/" + BUNDLED_CSS, DESTINATION + "/" + BUNDLED_HTML, DESTINATION + "/" + BUNDLED_JS, BUNDLED_SW]);
+    return del(['dist/style.css', 'dist/index.html', 'dist/script.js', 'sw.js']);
   }).task('bundle-css', function(){
     var css, images, i$, len$, image, image_path, base_name, hash;
-    css = fs.readFileSync(SOURCE_CSS + "", {
+    css = fs.readFileSync('css/style.css', {
       encoding: 'utf8'
     });
     images = css.match(IMAGES_REGEXP);
@@ -128,13 +114,13 @@
       image_path = image.substring(7, image.length - 1);
       base_name = image_path.split('/').pop();
       hash = file_hash(image_path);
-      css = css.replace(image, "url(" + DESTINATION + "/" + base_name + "?" + hash + ")");
-      fs.copyFileSync(image_path, DESTINATION + "/" + base_name);
+      css = css.replace(image, "url(dist/" + base_name + "?" + hash + ")");
+      fs.copyFileSync(image_path, "dist/" + base_name);
     }
-    fs.writeFileSync(DESTINATION + "/" + BUNDLED_CSS, css);
+    fs.writeFileSync('dist/style.css', css);
   }).task('bundle-html', ['generate-html'], function(){
     var html, fonts, i$, len$, font, font_path, base_name, hash, r, used_fa_icons, m, unused_fa_icons, definition, icon, glyph, js;
-    html = fs.readFileSync(DESTINATION + "/" + BUNDLED_HTML, {
+    html = fs.readFileSync('dist/index.html', {
       encoding: 'utf8'
     });
     html = html.replace(new RegExp('@font-face[^}]+(' + BLACKLISTED_FONTS.join('|') + ')[^}]+}', 'g'), '');
@@ -145,7 +131,7 @@
       base_name = font_path.split('/').pop();
       hash = file_hash(font_path);
       html = html.replace(font, "url(" + base_name + "?" + hash + ")");
-      fs.copyFileSync(font_path, DESTINATION + "/" + base_name);
+      fs.copyFileSync(font_path, "dist/" + base_name);
     }
     r = /icon="([^"]+)"/g;
     used_fa_icons = new Set;
@@ -169,12 +155,12 @@
       return string.substring(8, string.length - 9);
     }).join('');
     html = html.replace(/assetpath=".+?"/g, '').replace('<link rel="import" href="../node_modules/@polymer/shadycss/apply-shim.html">', '').replace('<link rel="import" href="../node_modules/@polymer/shadycss/custom-style-interface.html">', '').replace(SCRIPTS_REGEXP, '');
-    fs.writeFileSync(DESTINATION + "/" + BUNDLED_HTML, html);
-    fs.writeFileSync(DESTINATION + "/" + BUNDLED_JS, js);
+    fs.writeFileSync('dist/index.html', html);
+    fs.writeFileSync('dist/script.js', js);
   }).task('bundle-js', ['bundle-html'], function(){
     var config;
     config = Object.assign({
-      name: DESTINATION + "/" + BUNDLED_JS,
+      name: 'dist/script.js',
       optimize: 'none',
       onBuildRead: function(arg$, arg1$, contents){
         return contents.replace(/define\("([^"]+)".split\(" "\)/, function(arg$, dependencies){
@@ -186,20 +172,20 @@
         endFile: 'js/b.webcomponentsready-wrap-after.js'
       }
     }, requirejs_config);
-    return gulp.src(DESTINATION + "/" + BUNDLED_JS).pipe(gulpRequirejsOptimize(config)).pipe(gulp.dest(DESTINATION));
+    return gulp.src('dist/script.js').pipe(gulpRequirejsOptimize(config)).pipe(gulp.dest('dist'));
   }).task('bundle-service-worker', ['generate-service-worker'], function(){
     var sw;
-    fs.renameSync(DESTINATION + "/" + BUNDLED_SW, BUNDLED_SW);
-    sw = fs.readFileSync(BUNDLED_SW, {
+    fs.renameSync('dist/sw.js', 'sw.js');
+    sw = fs.readFileSync('sw.js', {
       encoding: 'utf8'
     });
-    sw = sw.replace(/importScripts\("/, "$&" + DESTINATION + "/").replace(/modulePathPrefix:\s*?"/, "$&" + DESTINATION + "/");
-    fs.writeFileSync(BUNDLED_SW, sw);
+    sw = sw.replace(/importScripts\("/, "$&dist/").replace(/modulePathPrefix:\s*?"/, "$&dist/");
+    fs.writeFileSync('sw.js', sw);
   }).task('clean', function(){
-    return del(DESTINATION + "/*");
+    return del('dist/*');
   }).task('copy-audio', ['bundle-js'], function(){
     var js, audios, i$, len$, audio, audio_path, base_name, hash;
-    js = fs.readFileSync(DESTINATION + "/" + BUNDLED_JS, {
+    js = fs.readFileSync('dist/script.js', {
       encoding: 'utf8'
     });
     audios = js.match(AUDIO_REGEXP);
@@ -208,21 +194,21 @@
       audio_path = audio.substring(1, audio.length - 1);
       base_name = audio_path.split('/').pop();
       hash = file_hash(audio_path);
-      js = js.replace(audio, "\"" + DESTINATION + "/" + base_name + "?" + hash + "\"");
-      fs.copyFileSync(audio_path, DESTINATION + "/" + base_name);
+      js = js.replace(audio, "\"dist/" + base_name + "?" + hash + "\"");
+      fs.copyFileSync(audio_path, "dist/" + base_name);
     }
-    fs.writeFileSync(DESTINATION + "/" + BUNDLED_JS, js);
+    fs.writeFileSync('dist/script.js', js);
   }).task('copy-favicon', function(){
-    fs.copyFileSync('favicon.ico', DESTINATION + "/favicon.ico");
+    fs.copyFileSync('favicon.ico', "dist/favicon.ico");
   }).task('copy-js', function(){
     var webcomponents;
     webcomponents = fs.readFileSync('node_modules/@webcomponents/webcomponentsjs/webcomponents-hi-sd-ce.js', {
       encoding: 'utf8'
     });
-    fs.writeFileSync(DESTINATION + "/webcomponents.min.js", minify_js(webcomponents));
+    fs.writeFileSync("dist/webcomponents.min.js", minify_js(webcomponents));
   }).task('copy-manifest', function(){
     var manifest, i$, ref$, len$, icon, base_name, hash;
-    manifest = JSON.parse(fs.readFileSync(SOURCE_MANIFEST + "", {
+    manifest = JSON.parse(fs.readFileSync('manifest.json', {
       encoding: 'utf8'
     }));
     manifest.start_url = '../' + manifest.start_url;
@@ -230,13 +216,13 @@
       icon = ref$[i$];
       base_name = icon.src.split('/').pop();
       hash = file_hash(icon.src);
-      fs.copyFileSync(icon.src, DESTINATION + "/" + base_name);
+      fs.copyFileSync(icon.src, "dist/" + base_name);
       icon.src = base_name + "?" + hash;
     }
-    fs.writeFileSync(DESTINATION + "/" + BUNDLED_MANIFEST, JSON.stringify(manifest));
+    fs.writeFileSync('dist/manifest.json', JSON.stringify(manifest));
   }).task('copy-wasm', ['bundle-js'], function(){
     var js, i$, ref$, len$, ref1$, name, location, main, hash;
-    js = fs.readFileSync(DESTINATION + "/" + BUNDLED_JS, {
+    js = fs.readFileSync('dist/script.js', {
       encoding: 'utf8'
     });
     for (i$ = 0, len$ = (ref$ = requirejs_config.packages).length; i$ < len$; ++i$) {
@@ -244,15 +230,15 @@
       if (name.endsWith('.wasm')) {
         hash = file_hash(location + "/src/" + name);
         js = js.replace("=\"" + name + "\"", "=\"" + name + "?" + hash + "\"");
-        fs.copyFileSync(location + "/src/" + name, DESTINATION + "/" + name);
+        fs.copyFileSync(location + "/src/" + name, "dist/" + name);
       }
     }
-    fs.writeFileSync(DESTINATION + "/" + BUNDLED_JS, js);
+    fs.writeFileSync('dist/script.js', js);
   }).task('default', function(callback){
     runSequence('clean', 'main-build', 'bundle-clean', 'minify-service-worker', 'bundle-clean', 'update-index', callback);
   }).task('generate-html', function(callback){
     var command;
-    command = "node_modules/.bin/polymer-bundler --strip-comments --rewrite-urls-in-templates --inline-css --inline-scripts --out-html " + DESTINATION + "/" + BUNDLED_HTML + " " + SOURCE_HTML;
+    command = "node_modules/.bin/polymer-bundler --strip-comments --rewrite-urls-in-templates --inline-css --inline-scripts --out-html dist/index.html html/index.html";
     exec(command, function(error, stdout, stderr){
       if (stdout) {
         console.log(stdout);
@@ -267,11 +253,11 @@
       cacheId: 'detox-chat-app',
       clientsClaim: true,
       globDirectory: '.',
-      globPatterns: [DESTINATION + "/*", 'index.html'],
+      globPatterns: ["dist/*", 'index.html'],
       ignoreUrlParametersMatching: [/./],
       importWorkboxFrom: 'local',
       skipWaiting: true,
-      swDest: DESTINATION + "/sw.js",
+      swDest: "dist/sw.js",
       manifestTransforms: [function(entries){
         var i$, len$, entry;
         for (i$ = 0, len$ = entries.length; i$ < len$; ++i$) {
@@ -288,24 +274,24 @@
     });
   }).task('main-build', ['copy-audio', 'copy-favicon', 'copy-js', 'copy-manifest', 'copy-wasm', 'minify-css', 'minify-html', 'minify-font', 'minify-js']).task('minify-css', ['bundle-css'], function(){
     var css;
-    css = fs.readFileSync(DESTINATION + "/" + BUNDLED_CSS, {
+    css = fs.readFileSync('dist/style.css', {
       encoding: 'utf8'
     });
-    fs.writeFileSync(DESTINATION + "/" + MINIFIED_CSS, minify_css(css));
+    fs.writeFileSync('dist/style.min.css', minify_css(css));
   }).task('minify-font', ['bundle-html'], function(callback){
     var font, css, command;
-    font = __dirname + ("/" + DESTINATION + "/" + FA_FONT);
-    css = __dirname + ("/" + DESTINATION + "/" + BUNDLED_HTML);
+    font = __dirname + '/dist/fa-solid-900.woff2';
+    css = __dirname + '/dist/index.html';
     command = "docker run --rm -v " + font + ":/font.woff2 -v " + css + ":/style.css nazarpc/subset-font";
     exec(command, function(error, stdout, stderr){
       var html, r, hash;
-      html = fs.readFileSync(DESTINATION + "/" + BUNDLED_HTML, {
+      html = fs.readFileSync('dist/index.html', {
         encoding: 'utf8'
       });
-      r = new RegExp(FA_FONT + "\\?\\w+");
+      r = new RegExp("fa-solid-900.woff2\\?\\w+");
       hash = file_hash(font);
-      html = html.replace(r, FA_FONT + "?" + hash);
-      fs.writeFileSync(DESTINATION + "/" + BUNDLED_HTML, html);
+      html = html.replace(r, "fa-solid-900.woff2?" + hash);
+      fs.writeFileSync('dist/index.html', html);
       stdout = stdout.replace("[INFO] Subsetting font '/tmp/font.ttf' with ebook '/tmp/characters' into new font '/tmp/font.ttf', containing the following glyphs:\n", '').replace('Processing /tmp/font.ttf => /tmp/font.woff2\n', '').trim();
       stderr = stderr.replace(/^The glyph named .+ is mapped to.+\n/gm, '').replace(/^But its name indicates it should be mapped to.+\n/gm, '').replace('Traceback (most recent call last):\n  File "/usr/bin/glyphIgo", line 1353, in <module>\n    main()\n  File "/usr/bin/glyphIgo", line 1344, in main\n    returnCode = GlyphIgo(args).execute()\n  File "/usr/bin/glyphIgo", line 1333, in execute\n    returnCode = self.__do_subset()\n  File "/usr/bin/glyphIgo", line 1307, in __do_subset\n    self.__print_char_list(found_char_list)\n  File "/usr/bin/glyphIgo", line 992, in __print_char_list\n    print "\'%s\'\\t%s\\t%s\\t%s" % (escape(c[0]), decCodePoint, hexCodePoint, name)\nUnicodeEncodeError: \'ascii\' codec can\'t encode character u\'\\uf00c\' in position 1: ordinal not in range(128)', '').replace(/^Compressed \d+ to \d+\.\n/gm, '').trim();
       if (stdout) {
@@ -317,21 +303,21 @@
       callback();
     });
   }).task('minify-html', ['bundle-html', 'minify-font'], function(){
-    return gulp.src(DESTINATION + "/" + BUNDLED_HTML).pipe(gulpHtmlmin({
+    return gulp.src('dist/index.html').pipe(gulpHtmlmin({
       decodeEntities: true,
       minifyCSS: minify_css,
       removeComments: true
-    })).pipe(gulpRename(MINIFIED_HTML)).pipe(gulp.dest(DESTINATION));
+    })).pipe(gulpRename('index.min.html')).pipe(gulp.dest('dist'));
   }).task('minify-js', ['bundle-js', 'copy-wasm'], function(){
     var js;
-    js = fs.readFileSync(DESTINATION + "/" + BUNDLED_JS, {
+    js = fs.readFileSync('dist/script.js', {
       encoding: 'utf8'
     });
     js = js.replace("typeof requirejs === 'function'", 'false').replace(/"function"===?typeof define&&define.amd/g, 'true');
-    fs.writeFileSync(DESTINATION + "/" + BUNDLED_JS, js);
-    return gulp.src(DESTINATION + "/" + BUNDLED_JS).pipe(uglify()).pipe(gulpRename(MINIFIED_JS)).pipe(gulp.dest(DESTINATION));
+    fs.writeFileSync('dist/script.js', js);
+    return gulp.src('dist/script.js').pipe(uglify()).pipe(gulpRename('script.min.js')).pipe(gulp.dest('dist'));
   }).task('minify-service-worker', ['bundle-service-worker'], function(){
-    return gulp.src(BUNDLED_SW).pipe(uglify()).pipe(gulpRename(MINIFIED_SW)).pipe(gulp.dest('.'));
+    return gulp.src('sw.js').pipe(uglify()).pipe(gulpRename('sw.min.js')).pipe(gulp.dest('.'));
   }).task('update-index', function(){
     var index, critical_css, files_for_hash_update, i$, len$, file, hash;
     index = fs.readFileSync('index.html', {
@@ -341,7 +327,7 @@
       encoding: 'utf8'
     }).trim();
     index = index.replace(/<style>.*?<\/style>/g, "<style>" + critical_css + "</style>");
-    files_for_hash_update = [DESTINATION + "/" + MINIFIED_CSS, DESTINATION + "/favicon.ico", DESTINATION + "/" + MINIFIED_HTML, DESTINATION + "/" + MINIFIED_JS, DESTINATION + "/" + BUNDLED_MANIFEST, DESTINATION + "/webcomponents.min.js"];
+    files_for_hash_update = ['dist/style.min.css', 'dist/favicon.ico', 'dist/index.min.html', 'dist/script.min.js', 'dist/manifest.json', 'dist/webcomponents.min.js'];
     for (i$ = 0, len$ = files_for_hash_update.length; i$ < len$; ++i$) {
       file = files_for_hash_update[i$];
       hash = file_hash(file);
--- gulpfile.ls
+++ gulpfile.ls
@@ -46,21 +46,6 @@ const BLACKLISTED_FONTS	= [
 	'fa-regular-400.woff2'
 	'fa-brands-400.woff2'
 ]
-const FA_FONT			= 'fa-solid-900.woff2'
-
-const BUNDLED_CSS		= 'style.css'
-const BUNDLED_HTML		= 'index.html'
-const BUNDLED_JS		= 'script.js'
-const BUNDLED_MANIFEST	= 'manifest.json'
-const BUNDLED_SW		= 'sw.js'
-const DESTINATION		= 'dist'
-const MINIFIED_CSS		= 'style.min.css'
-const MINIFIED_HTML		= 'index.min.html'
-const MINIFIED_JS		= 'script.min.js'
-const MINIFIED_SW		= 'sw.min.js'
-const SOURCE_CSS		= 'css/style.css'
-const SOURCE_HTML		= 'html/index.html'
-const SOURCE_MANIFEST	= 'manifest.json'
 
 requirejs_config	=
 	'baseUrl'	: '.'
@@ -117,21 +102,21 @@ requirejs_config	=
 
 gulp
 	.task('bundle-clean', ->
-		del(["#DESTINATION/#BUNDLED_CSS", "#DESTINATION/#BUNDLED_HTML", "#DESTINATION/#BUNDLED_JS", BUNDLED_SW])
+		del(['dist/style.css', 'dist/index.html', 'dist/script.js', 'sw.js'])
 	)
 	.task('bundle-css', !->
-		css		= fs.readFileSync("#SOURCE_CSS", {encoding: 'utf8'})
+		css		= fs.readFileSync('css/style.css', {encoding: 'utf8'})
 		images	= css.match(IMAGES_REGEXP)
 		for image in images
 			image_path	= image.substring(7, image.length - 1)
 			base_name	= image_path.split('/').pop()
 			hash		= file_hash(image_path)
-			css			= css.replace(image, "url(#DESTINATION/#base_name?#hash)")
-			fs.copyFileSync(image_path, "#DESTINATION/#base_name")
-		fs.writeFileSync("#DESTINATION/#BUNDLED_CSS", css)
+			css			= css.replace(image, "url(dist/#base_name?#hash)")
+			fs.copyFileSync(image_path, "dist/#base_name")
+		fs.writeFileSync('dist/style.css', css)
 	)
 	.task('bundle-html', ['generate-html'], !->
-		html	= fs.readFileSync("#DESTINATION/#BUNDLED_HTML", {encoding: 'utf8'})
+		html	= fs.readFileSync('dist/index.html', {encoding: 'utf8'})
 		# Hack: we know for sure that these files fonts will not be used, so lets remove unused fonts from stylesheet entirely
 		html	= html.replace(new RegExp('@font-face[^}]+(' + BLACKLISTED_FONTS.join('|') + ')[^}]+}', 'g'), '')
 		fonts	= html.match(FONTS_REGEXP)
@@ -140,7 +125,7 @@ gulp
 			base_name	= font_path.split('/').pop()
 			hash		= file_hash(font_path)
 			html		= html.replace(font, "url(#base_name?#hash)")
-			fs.copyFileSync(font_path, "#DESTINATION/#base_name")
+			fs.copyFileSync(font_path, "dist/#base_name")
 		r				= /icon="([^"]+)"/g
 		used_fa_icons	= new Set
 		while m = r.exec(html)
@@ -167,12 +152,12 @@ gulp
 			.replace('<link rel="import" href="../node_modules/@polymer/shadycss/custom-style-interface.html">', '')
 			# Remove all <script> tags, we'll have them included separately
 			.replace(SCRIPTS_REGEXP, '')
-		fs.writeFileSync("#DESTINATION/#BUNDLED_HTML", html)
-		fs.writeFileSync("#DESTINATION/#BUNDLED_JS", js)
+		fs.writeFileSync('dist/index.html', html)
+		fs.writeFileSync('dist/script.js', js)
 	)
 	.task('bundle-js', ['bundle-html'], ->
 		config	= Object.assign({
-			name					: "#DESTINATION/#BUNDLED_JS"
+			name					: 'dist/script.js'
 			optimize				: 'none'
 			onBuildRead				: (, , contents) ->
 				# Hack: Workaround for https://github.com/google/closure-compiler/issues/2953
@@ -183,65 +168,65 @@ gulp
 				startFile	: ['js/a.require.js', 'js/b.webcomponentsready-wrap-before.js']
 				endFile		: 'js/b.webcomponentsready-wrap-after.js'
 		}, requirejs_config)
-		gulp.src("#DESTINATION/#BUNDLED_JS")
+		gulp.src('dist/script.js')
 			.pipe(gulp-requirejs-optimize(config))
-			.pipe(gulp.dest(DESTINATION))
+			.pipe(gulp.dest('dist'))
 	)
 	.task('bundle-service-worker', ['generate-service-worker'], !->
-		fs.renameSync("#DESTINATION/#BUNDLED_SW", BUNDLED_SW)
-		sw	= fs.readFileSync(BUNDLED_SW, {encoding: 'utf8'})
+		fs.renameSync('dist/sw.js', 'sw.js')
+		sw	= fs.readFileSync('sw.js', {encoding: 'utf8'})
 		# TODO: Hack, should be possible to remove in future when https://github.com/GoogleChrome/workbox/pull/1403 lands in stable version
 		sw	= sw
-			.replace(/importScripts\("/, "$&#DESTINATION/")
-			.replace(/modulePathPrefix:\s*?"/, "$&#DESTINATION/")
-		fs.writeFileSync(BUNDLED_SW, sw)
+			.replace(/importScripts\("/, "$&dist/")
+			.replace(/modulePathPrefix:\s*?"/, "$&dist/")
+		fs.writeFileSync('sw.js', sw)
 	)
 	.task('clean', ->
-		del("#DESTINATION/*")
+		del('dist/*')
 	)
 	.task('copy-audio', ['bundle-js'], !->
-		js		= fs.readFileSync("#DESTINATION/#BUNDLED_JS", {encoding: 'utf8'})
+		js		= fs.readFileSync('dist/script.js', {encoding: 'utf8'})
 		audios	= js.match(AUDIO_REGEXP)
 		for audio in audios
 			audio_path	= audio.substring(1, audio.length - 1)
 			base_name	= audio_path.split('/').pop()
 			hash		= file_hash(audio_path)
-			js			= js.replace(audio, "\"#DESTINATION/#base_name?#hash\"")
-			fs.copyFileSync(audio_path, "#DESTINATION/#base_name")
-		fs.writeFileSync("#DESTINATION/#BUNDLED_JS", js)
+			js			= js.replace(audio, "\"dist/#base_name?#hash\"")
+			fs.copyFileSync(audio_path, "dist/#base_name")
+		fs.writeFileSync('dist/script.js', js)
 	)
 	.task('copy-favicon', !->
-		fs.copyFileSync('favicon.ico', "#DESTINATION/favicon.ico")
+		fs.copyFileSync('favicon.ico', "dist/favicon.ico")
 	)
 	.task('copy-js', !->
 		webcomponents	= fs.readFileSync('node_modules/@webcomponents/webcomponentsjs/webcomponents-hi-sd-ce.js', {encoding: 'utf8'})
-		fs.writeFileSync("#DESTINATION/webcomponents.min.js", minify_js(webcomponents))
+		fs.writeFileSync("dist/webcomponents.min.js", minify_js(webcomponents))
 	)
 	.task('copy-manifest', !->
-		manifest			= JSON.parse(fs.readFileSync("#SOURCE_MANIFEST", {encoding: 'utf8'}))
+		manifest			= JSON.parse(fs.readFileSync('manifest.json', {encoding: 'utf8'}))
 		manifest.start_url	= '../' + manifest.start_url
 		for icon in manifest.icons
 			base_name	= icon.src.split('/').pop()
 			hash		= file_hash(icon.src)
-			fs.copyFileSync(icon.src, "#DESTINATION/#base_name")
+			fs.copyFileSync(icon.src, "dist/#base_name")
 			icon.src	= "#base_name?#hash"
-		fs.writeFileSync("#DESTINATION/#BUNDLED_MANIFEST", JSON.stringify(manifest))
+		fs.writeFileSync('dist/manifest.json', JSON.stringify(manifest))
 	)
 	.task('copy-wasm', ['bundle-js'], !->
-		js	= fs.readFileSync("#DESTINATION/#BUNDLED_JS", {encoding: 'utf8'})
+		js	= fs.readFileSync('dist/script.js', {encoding: 'utf8'})
 		for {name, location, main} in requirejs_config.packages
 			if name.endsWith('.wasm')
 				hash	= file_hash("#location/src/#name")
 				# This is a hack, but it works for now
 				js		= js.replace("=\"#name\"", "=\"#name?#hash\"")
-				fs.copyFileSync("#location/src/#name", "#DESTINATION/#name")
-		fs.writeFileSync("#DESTINATION/#BUNDLED_JS", js)
+				fs.copyFileSync("#location/src/#name", "dist/#name")
+		fs.writeFileSync('dist/script.js', js)
 	)
 	.task('default', (callback) !->
 		run-sequence('clean', 'main-build', 'bundle-clean', 'minify-service-worker', 'bundle-clean', 'update-index', callback)
 	)
 	.task('generate-html', (callback) !->
-		command		= "node_modules/.bin/polymer-bundler --strip-comments --rewrite-urls-in-templates --inline-css --inline-scripts --out-html #DESTINATION/#BUNDLED_HTML #SOURCE_HTML"
+		command		= "node_modules/.bin/polymer-bundler --strip-comments --rewrite-urls-in-templates --inline-css --inline-scripts --out-html dist/index.html html/index.html"
 		exec(command, (error, stdout, stderr) !->
 			if stdout
 				console.log(stdout)
@@ -256,13 +241,13 @@ gulp
 			clientsClaim				: true
 			globDirectory				: '.'
 			globPatterns				: [
-				"#DESTINATION/*"
+				"dist/*"
 				'index.html'
 			]
 			ignoreUrlParametersMatching	: [/./]
 			importWorkboxFrom			: 'local'
 			skipWaiting					: true
-			swDest						: "#DESTINATION/sw.js"
+			swDest						: "dist/sw.js"
 			manifestTransforms			: [
 				(entries) ->
 					for entry in entries
@@ -275,21 +260,21 @@ gulp
 	)
 	.task('main-build', ['copy-audio', 'copy-favicon', 'copy-js', 'copy-manifest', 'copy-wasm', 'minify-css', 'minify-html', 'minify-font', 'minify-js'])
 	.task('minify-css', ['bundle-css'], !->
-		css	= fs.readFileSync("#DESTINATION/#BUNDLED_CSS", {encoding: 'utf8'})
-		fs.writeFileSync("#DESTINATION/#MINIFIED_CSS", minify_css(css))
+		css	= fs.readFileSync('dist/style.css', {encoding: 'utf8'})
+		fs.writeFileSync('dist/style.min.css', minify_css(css))
 	)
 	.task('minify-font', ['bundle-html'], (callback) !->
-		font	= __dirname + "/#DESTINATION/#FA_FONT"
-		css		= __dirname + "/#DESTINATION/#BUNDLED_HTML"
+		font	= __dirname + '/dist/fa-solid-900.woff2'
+		css		= __dirname + '/dist/index.html'
 		command	= "docker run --rm -v #font:/font.woff2 -v #css:/style.css nazarpc/subset-font"
 		exec(command, (error, stdout, stderr) !->
 			# We need to update hash for this font, since file contents have changed
-			html	= fs.readFileSync("#DESTINATION/#BUNDLED_HTML", {encoding: 'utf8'})
-			r		= new RegExp("#FA_FONT\\?\\w+")
+			html	= fs.readFileSync('dist/index.html', {encoding: 'utf8'})
+			r		= new RegExp("fa-solid-900.woff2\\?\\w+")
 			hash	= file_hash(font)
 			html	= html
-				.replace(r, "#FA_FONT?#hash")
-			fs.writeFileSync("#DESTINATION/#BUNDLED_HTML", html)
+				.replace(r, "fa-solid-900.woff2?#hash")
+			fs.writeFileSync('dist/index.html', html)
 			# Remove some known harmless output
 			stdout	= stdout
 				.replace("[INFO] Subsetting font '/tmp/font.ttf' with ebook '/tmp/characters' into new font '/tmp/font.ttf', containing the following glyphs:\n", '')
@@ -311,31 +296,31 @@ gulp
 		)
 	)
 	.task('minify-html', ['bundle-html', 'minify-font'], ->
-		gulp.src("#DESTINATION/#BUNDLED_HTML")
+		gulp.src('dist/index.html')
 			.pipe(gulp-htmlmin(
 				decodeEntities	: true
 				minifyCSS		: minify_css
 				removeComments	: true
 			))
-			.pipe(gulp-rename(MINIFIED_HTML))
-			.pipe(gulp.dest(DESTINATION))
+			.pipe(gulp-rename('index.min.html'))
+			.pipe(gulp.dest('dist'))
 	)
 	.task('minify-js', ['bundle-js', 'copy-wasm'], ->
 		# First couple of manual optimizations
-		js	= fs.readFileSync("#DESTINATION/#BUNDLED_JS", {encoding: 'utf8'})
+		js	= fs.readFileSync('dist/script.js', {encoding: 'utf8'})
 		js	= js
 			.replace("typeof requirejs === 'function'", 'false')
 			.replace(/"function"===?typeof define&&define.amd/g, 'true')
-		fs.writeFileSync("#DESTINATION/#BUNDLED_JS", js)
-		gulp.src("#DESTINATION/#BUNDLED_JS")
+		fs.writeFileSync('dist/script.js', js)
+		gulp.src('dist/script.js')
 			.pipe(uglify())
-			.pipe(gulp-rename(MINIFIED_JS))
-			.pipe(gulp.dest(DESTINATION))
+			.pipe(gulp-rename('script.min.js'))
+			.pipe(gulp.dest('dist'))
 	)
 	.task('minify-service-worker', ['bundle-service-worker'], ->
-		gulp.src(BUNDLED_SW)
+		gulp.src('sw.js')
 			.pipe(uglify())
-			.pipe(gulp-rename(MINIFIED_SW))
+			.pipe(gulp-rename('sw.min.js'))
 			.pipe(gulp.dest('.'))
 	)
 	.task('update-index', !->
@@ -343,12 +328,12 @@ gulp
 		critical_css			= fs.readFileSync('css/critical.css', {encoding: 'utf8'}).trim()
 		index					= index.replace(/<style>.*?<\/style>/g, "<style>#critical_css</style>")
 		files_for_hash_update	= [
-			"#DESTINATION/#MINIFIED_CSS"
-			"#DESTINATION/favicon.ico"
-			"#DESTINATION/#MINIFIED_HTML"
-			"#DESTINATION/#MINIFIED_JS"
-			"#DESTINATION/#BUNDLED_MANIFEST"
-			"#DESTINATION/webcomponents.min.js"
+			'dist/style.min.css'
+			'dist/favicon.ico'
+			'dist/index.min.html'
+			'dist/script.min.js'
+			'dist/manifest.json'
+			'dist/webcomponents.min.js'
 		]
 		for file in files_for_hash_update
 			hash	= file_hash(file)
